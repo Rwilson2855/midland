4ae21bf1b4b8c71a8f9f4fb4604c961979378c35
Rwilson2855 <rwilson2855@gmail.com>
2024-11-19 19:31:21 -0600

    what is going on

---
+++ src/html module/index.js
@@ -0,0 +1,38 @@
+// Logic Practice
+
+// 1. Positive or Negative
+// Create a program that checks if a number stored in a variable is positive or negative.
+// Log "The number is positive" if it's greater than zero, otherwise log "The number is negative."
+if (i > 0) {
+    console.log("The number is positive");
+} else if (i < 0) {
+    console.log ("The number is negative")
+}
+// 2. Minimum Age for Driving
+// Write a program that checks a person's age stored in a variable and logs "Can drive" if they are 16 or older.
+// If they are under 16, log "Cannot drive."
+const legalDrivingAge = 16
+let age = 18
+if (age >= 16) {
+    console.log("Can drive");
+} else if (age < 16) {
+    console.log ("Cannot drive")
+}
+// 3. School Grade Levels
+// Create a program that categorizes school levels based on a student's grade stored in a variable.
+// Use the following categories: "Elementary" for grades 1-5, "Middle" for grades 6-8, and "High" for grades 9-12.
+// Log the school level.
+if (i = [1,2,3,4,5]) {
+    console.log ("Elementary")
+}
+else if (i = [6,7,8]) {
+    console.log ("Middle")
+}
+else if(i = [9,10,11,12]) {
+    console.log ("high")
+}
+
+// 4. Basic Discount System
+// A shop offers a discount for purchases over $50.
+// Write a program where the total purchase amount is stored in a variable.
+// If the amount is over $50, calculate the discount and log it. Otherwise, log "No discount available."
