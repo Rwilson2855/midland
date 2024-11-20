8113a361a1eea6f3772c7161192e5924e4a9e551
Rwilson2855 <rwilson2855@gmail.com>
2024-11-20 16:35:23 -0600

    idk what changed but it was probably something

--- src/js module/dayone.js
+++ src/js module/dayone.js
@@ -12,7 +12,7 @@ if (i > 0) {
 // Write a program that checks a person's age stored in a variable and logs "Can drive" if they are 16 or older.
 // If they are under 16, log "Cannot drive."
 const legalDrivingAge = 16
-let age = 18
+const age = 18
 if (age >= 16) {
     console.log("Can drive");
 } else if (age < 16) {
@@ -36,3 +36,21 @@ else if(i = [9,10,11,12]) {
 // A shop offers a discount for purchases over $50.
 // Write a program where the total purchase amount is stored in a variable.
 // If the amount is over $50, calculate the discount and log it. Otherwise, log "No discount available."
+// FIZZBUZZ
+
+// Run through all the numbers from 1-100
+// Log the number and "FizzBuzz" if the number is divisible by 3 and 5
+// Log the number and "Fizz" if the number is divisible by 3
+// Log the number and "Buzz" if the number is divisible by 5
+// Log the number if none of the above conditions are true
+const maxNumber = prompt("Enter a fizzbuzzy number: ");
+for (let i = 1; i < 100; i++) {
+    const printFIZZBUZZ = i%3 === 0 && i%5 === 0
+    const FIZZBUZZ = printFIZZBUZZ ? 'FizzBuzz' : ''
+    console.log('${');
+
+}
+// EXTRA:
+
+// Modify the FizzBuzz problem to allow the user to choose a max integer.
+// (example: the user is prompted for a number, they choose 15, so the problem runs through the numbers 1 to 15)
---
+++ src/js module/daytwo.js
@@ -0,0 +1,4 @@
+let userInput = prompt("How was your day")
+if (userInput.length >=4) {
+    console.log(userInput.slice(3));
+}
