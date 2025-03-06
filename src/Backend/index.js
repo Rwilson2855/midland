d110a762740f1bb5b48c54988fb9dadecfc31083
Rwilson2855 <rwilson2855@gmail.com>
2025-03-05 20:57:52 -0600

    server set up

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -1,7 +1,19 @@
 const port = 3000;
 const express = require('express');
 const app = express();
-app.get('/' (req, res) => {
+const posts = require('./posts.json')
+app.get('/', (_req, res) => {
     res.send('Hello World!');
 });
-app.listen(port)
+app.get('/users/:id', function (req, res) {
+    const userId = parseInt(req.params.id);
+    const userPosts = posts.filter (post => post.userId === userId);
+    res.json(userPosts);
+  });
+app.all('*', (_req, res) => {
+    res.redirect('/');
+});
+
+
+
+app.listen(port, () => console.log(`Example app listening on ${port}!`));
